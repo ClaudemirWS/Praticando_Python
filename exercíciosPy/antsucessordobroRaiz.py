62b008a38d7e51120d67d707e826c5c1372aeb16
ClaudemirWS <claudemirwss@gmail.com>
2021-09-29 18:06:33 -0300

    add coments to codes

--- exercíciosPy/antsucessordobroRaiz.py
+++ exercíciosPy/antsucessordobroRaiz.py
@@ -1,6 +1,8 @@
 print(' ========== ANTECESSOR, SUCESSOR, DOBRO, TRIPLO E RAIZ QUADRADA ========== ')
-n = int(input('Digite um número: '))
+n = int(input('Digite um número: ')) #recebe número
 
-print('\nVocê digitou {}, o antecessor é {} e o sucessor é {}\n'.format(n,n-1,n+1))
+#utiliza soma e sub para printar antecessor e sucessor 
+print('\nVocê digitou {}, o antecessor é {} e o sucessor é {}\n'.format(n,n-1,n+1)) 
 
+#printa dobro, triplo e raiz quadrada do número
 print('O dobro deste número é {}, o triplo é {} e a raiz quadrada é {:.2f}\n'.format(n*2,n*3,n**(1/2)))
