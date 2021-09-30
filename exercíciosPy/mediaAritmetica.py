40d0366d1d6e78c61711a5e0972efa72a1411235
ClaudemirWS <claudemirwss@gmail.com>
2021-09-30 19:03:52 -0300

    add mediaAritmetica

---
+++ exercíciosPy/mediaAritmetica.py
@@ -0,0 +1,5 @@
+#Código simples de media para utilização do tipo FLOAT e ordem de procedência
+print('========== CALCULO DE MÉDIA ==========\n')
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+print('\nA sua média aritmética é: {:.1f}'.format((n1+n2)/2))
