094f579d6c32047cf63eb1aa48c484c23ff9bac6
ClaudemirWS <claudemirwss@gmail.com>
2021-10-01 16:20:42 -0300

    add conversorDeMoedas

---
+++ exercíciosPy/conversorDeMoedas.py
@@ -0,0 +1,3 @@
+print('========== CONVERSOR DE MOEDAS ==========')
+v = float(input('Digite o valor que deseja converter em R$: '))
+print('\nO valor de {} R$ em dolar é: {:.2f} US$ '.format(v, v/5.36))
