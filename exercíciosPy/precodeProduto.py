f4e753310d65602aeb8017abd21ed0df7fa15eaf
ClaudemirWS <claudemirwss@gmail.com>
2021-10-04 18:44:45 -0300

    add precodeProduto

---
+++ exercíciosPy/precodeProduto.py
@@ -0,0 +1,16 @@
+print('========= DIFERENÇA NO FALOR FINAL DE UM PRODUTO ==========')
+prec = float(input('Qual o preço do produto em R$? '))
+parce = int(input('Em quantas parcelas deseja pagar? '))
+
+if (parce == 1 or parce == 0):
+    avista = prec - (prec*5/100)    
+    print('\nO valor final do produto a vista ficará R$ {:.2f} com 5%'' de desconto.'.format(avista))
+elif (parce == 2):
+    aumen = prec + (prec*5/100)
+    print('\nO valor final do produto parcelado em {}x ficará R$ {:.2f} com 5%'' de aumento no valor total.'.format(parce,aumen))
+elif (parce == 3):
+    aumen = prec + (prec*10/100)
+    print('\nO valor final do produto parcelado em {}x ficará R$ {:.2f} com 10%'' de aumento no valor total.'.format(parce,aumen))
+else:
+    print('\nTente novamente..')
+
