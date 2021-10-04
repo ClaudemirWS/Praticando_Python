3c665d40bc7a2578e224c2d3761ade775a4912f2
ClaudemirWS <claudemirwss@gmail.com>
2021-10-04 14:41:59 -0300

    add comentaries to pintandoParede

--- exercíciosPy/pintandoParede.py
+++ exercíciosPy/pintandoParede.py
@@ -3,5 +3,6 @@ lar = float(input('Digite a largura da parede em metros: '))
 alt = float(input('Digite a altura da parede em metros: '))
 area = alt * lar
 print('\nSua parede tem a dimensão de {}x{} e sua área equivale a {}m²\n'.format(lar,alt,area))
+#cada litro de tinta pinta uma área de 2 metros quadrados, por isso divisão por 2
 print('Para pintar essa parede, você precisará de {} litros de tinta!\n'.format(area/2))
 
