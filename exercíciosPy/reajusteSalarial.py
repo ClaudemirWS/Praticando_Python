02028d54f9e3194e63ed0430189bb2e0249decd3
ClaudemirWS <claudemirwss@gmail.com>
2021-10-04 18:49:27 -0300

    update reajusteSalarial

--- exercíciosPy/reajusteSalarial.py
+++ exercíciosPy/reajusteSalarial.py
@@ -1,6 +1,6 @@
 print('========= APLICANDO AUMENTO DE 15% NO SALÁRIO ==========')
 sal = float(input('Digite o seu salário atual: '))
-aum = sal + (sal*15/100) #faz calculo em cima de 15%
+aum = sal + (sal*15/100) #faz calculo em cima de 15% e soma ao salário
 print('\nO seu salário de R${:.2f} e com o aumento irá somar R${:.2f}'.format(sal,aum))
 
 
