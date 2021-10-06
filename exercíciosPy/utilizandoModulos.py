a869896aa2f67c20656e62d03acd8661e1e2b7a4
ClaudemirWS <claudemirwss@gmail.com>
2021-10-06 10:45:34 -0300

    add moduloMath

--- exercíciosPy/utilizandoModulos.py
+++
@@ -1,4 +0,0 @@
-from math import trunc #aprendendo a importar modulos
-print('========== MOSTRA PARTE INTEIRA DE UM NÚMERO REAL =========')
-num = float(input('Digite um número real qualquer: '))
-print('O que vem antes da virgula no número digitado é {}'.format(trunc(num)))
