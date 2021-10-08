21dbcebd4e745101bdbdfa8d973b39cd07f89ae8
ClaudemirWS <claudemirwss@gmail.com>
2021-10-08 18:06:43 -0300

    add praticandoStrings

---
+++ exercíciosPy/praticandoStrings.py
@@ -0,0 +1,12 @@
+frase = '\nPraticando Strings'
+texto = """
+Praticando a escrita de frases e textos
+em um único print separado por linhas 
+diferentes mas com continuidade do
+conteúdo apresentado na tela, além disso,
+estou testando algumas funções.
+"""
+
+print('-'.join(frase[0:19]))
+print(texto.title().replace('Praticando', 'Estudando'))
+
--- exercíciosPy/tocandomp3.py
+++ exercíciosPy/tocandomp3.py
@@ -1,7 +1,7 @@
-import pygame
+import pygame #importa biblioteca pygame
 
 pygame.init()
-pygame.mixer.music.load('tocandomp3.mp3')
-pygame.mixer.music.play()
-input('Pressione ENTER para terminar a música')
+pygame.mixer.music.load('tocandomp3.mp3') #carrega música pelo nome
+pygame.mixer.music.play() #inicia reprodução
+input('Pressione ENTER para terminar a música') #termina a redrodução mediante input
 
