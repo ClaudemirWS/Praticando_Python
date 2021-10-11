9c7f8359a52bffd41167394bf796967507ba7a74
ClaudemirWS <claudemirwss@gmail.com>
2021-10-11 13:14:44 -0300

    rename files and add praticandoStrings4

---
+++ exercíciosPy/praticandoStrings4.py
@@ -0,0 +1,7 @@
+print("========== ANALISANDO NOME DE CIDADE ==========")
+cidade = str(input('Digite o nome de uma cidade: ')).strip().lower()
+print('A cidade possui a palavra "Santo" ou "São"? {}'.format('santo' in cidade or 'são' in cidade))
+
+
+
+
