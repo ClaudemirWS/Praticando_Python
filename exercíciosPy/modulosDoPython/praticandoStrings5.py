f0468732e14d7262ca71babdf5eddad0ec5cf875
ClaudemirWS <claudemirwss@gmail.com>
2021-10-11 13:22:32 -0300

    create folders

---
+++ exercíciosPy/modulosDoPython/praticandoStrings5.py
@@ -0,0 +1,8 @@
+print('========== Procurando palavras em nome ==========')
+nome = str(input('Digite seu nome completo: ')).strip().lower()
+print('Seu nome tem Silva? {}'.format('silva' in nome))
+
+
+
+
+
