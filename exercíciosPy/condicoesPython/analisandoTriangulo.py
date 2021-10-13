854724c80ef537c64cf57c365f6fcb21025f62f9
ClaudemirWS <claudemirwss@gmail.com>
2021-10-13 16:34:10 -0300

    lidandoComCores

--- exercíciosPy/condicoesPython/analisandoTriangulo.py
+++ exercíciosPy/condicoesPython/analisandoTriangulo.py
@@ -3,7 +3,7 @@ print('Digite três segmentos para vermos a possibilidade de um TRIÂNGULO.\n')
 s1 = float(input('Digite o primeiro segmento: '))
 s2 = float(input('Digite o segundo segmento: '))
 s3 = float(input('Digite o terceiro segmento: '))
-
+#um segmento precisa ser menor do que a soma dos outros dois
 if (s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2):
     print('{} com {} com {} PODEM formar um TRIÂNGULO.'.format(s1,s2,s3))
 else:
