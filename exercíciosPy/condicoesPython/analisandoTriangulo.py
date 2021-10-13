877779f2d828a3b820d7c717e2ade8e52acb5c45
ClaudemirWS <claudemirwss@gmail.com>
2021-10-13 14:11:10 -0300

    praticandoPython

---
+++ exercíciosPy/condicoesPython/analisandoTriangulo.py
@@ -0,0 +1,11 @@
+print('========== ANALISAOR DE TRIÂNGULOS ==========')
+print('Digite três segmentos para vermos a possibilidade de um TRIÂNGULO.\n')
+s1 = float(input('Digite o primeiro segmento: '))
+s2 = float(input('Digite o segundo segmento: '))
+s3 = float(input('Digite o terceiro segmento: '))
+
+if (s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2):
+    print('{} com {} com {} PODEM formar um TRIÂNGULO.'.format(s1,s2,s3))
+else:
+    print('\n{} com {} com {} NÃO PODEM formar um TRIÂNGULO.'.format(s1,s2,s3))
+
