2f8a0cd464c2dac3250eeefad1f8c2c3491bf54c
ClaudemirWS <claudemirwss@gmail.com>
2021-10-19 19:14:06 -0300

    add contagemRegressiva

---
+++ exercíciosPy/repeticoesPython/contagemRegressiva.py
@@ -0,0 +1,9 @@
+import time
+print('========== CONTAGEM REGRESSIVA ==========')
+num = 10
+for i in range (1,10):  #laço de repetição 
+   print('Ano Novo em {}'.format(num))
+   time.sleep(1) #reduz processamento em 1 segundo
+   num = num - 1 #contagem regressiva
+   if (num == 1):
+       print('\033[031mFELIZ ANO NOVO!!\033[m')
