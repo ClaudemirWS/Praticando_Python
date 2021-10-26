6d8c4db78b2e34316544ebe5729ec90df64689ba
ClaudemirWS <claudemirwss@gmail.com>
2021-10-26 15:14:28 -0300

    update eleicaoConselhoMoradores

--- exercíciosPy/Exercícios Próprios/eleicaoConselhoMoradores.py
+++ exercíciosPy/Exercícios Próprios/eleicaoConselhoMoradores.py
@@ -1,7 +1,9 @@
 lista = []
+listavotos = []
 cod = 0
 inicia = 0
-voto = 0
+votar = 1
+numvoto = 0
 votado = ''
 
 print('===== ELEIÇÃO DO CONSELHO DE MORADORES DO BAIRRO =====')
@@ -12,7 +14,7 @@ for count in range (1, cand+1):#REGISTRA CANDIDATOS
     pessoa = str(input('Registre o nome do {}º candidato: '.format(count))).strip().title()  
     cod = int(input('Registre o código de votação do {}º candidato: '.format(count)))    
     lista += [[cod,pessoa]]
-
+'''
 ver = int(input('\nVer a lista de candidatos? 1 - SIM ou 2 - NÃO: '))
 if (ver == 1):#LISTAGEM DE CANDIDADOS
     print('\n===== LISTA DE CANDIDATOS =====')
@@ -27,39 +29,47 @@ if (ver == 1):#LISTAGEM DE CANDIDADOS
             num = candidato[0] 
             nome = candidato[1]                  
         print('\nCandidato: {}, vote no nº {}.'.format(nome,num))
-    ver = 2
+'''    
 
-if(ver == 2):#INICIAR VOTAÇÃO?
-    inicia = int(input('\nIniciar votação? 1 - SIM ou 2 - NÃO: ')) 
+inicia = int(input('\nIniciar votação? 1 - SIM ou 2 - NÃO: ')) 
 
 if(inicia == 1):#VOTAÇÃO
     print('\n===== VOTAÇÃO =====')
-    escolha = int(input('Digite o número do seu candidato: '))
-    for count in range (0,len(lista)):
-        if (count == 0):
-            candidato = lista[count]
-            num = candidato[0]
-            nome = candidato[1]            
-        else:                                        
-            candidato = lista[count]
-            num = candidato[0]
-            nome = candidato[1]        
+    while inicia == 1: #roda o programa
+        escolha = int(input('Digite o número do seu candidato: '))
+        for count in range (0,len(lista)):
+            if (count == 0):
+                candidato = lista[count]
+                num = candidato[0]
+                nome = candidato[1]            
+            else:                                        
+                candidato = lista[count]
+                num = candidato[0]
+                nome = candidato[1]        
 
-        if (count == 0):
-            if (escolha == num):
-                voto = num
-                votado = nome
+            if (count == 0):
+                if (escolha == num):
+                    numvoto = num
+                    votado = nome                    
+            else: 
+                if (escolha == num):
+                    numvoto = num
+                    votado = nome
+        
+        listavotos += [numvoto]#registra número de votos em uma lista                                    
+        
+        if (numvoto != 0):    
+            print('Você votou em {}, nº {}.'.format(votado,numvoto))                      
         else: 
-            if (escolha == num):
-                voto = num
-                votado = nome   
-                    
-    if (voto != 0):    
-        print('Você votou em {}, nº {}.'.format(votado,voto)) 
-    else: 
-        print('Você votou NULO.')
-          
-elif(inicia == 2):#FINALIZA PROGRAMA CASO DESEJE NÃO VOTAR 
+            print('Você votou NULO.')
+
+        numvoto = 0#zera o último votado
+
+        inicia = int(input('\nDeseja votar novamente? 1 - SIM ou 2 - NÃO: '))      
+
+print('Lista de votos: {}'.format(listavotos))
+
+if(inicia == 2):#FINALIZA PROGRAMA CASO DESEJE NÃO VOTAR 
     print('\nFinalizando...') 
     exit()
 
