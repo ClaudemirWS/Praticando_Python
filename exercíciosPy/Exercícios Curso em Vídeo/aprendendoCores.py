1580ea6e31233d72b48279a61b6ccbb1adf71d15
ClaudemirWS <claudemirwss@gmail.com>
2021-10-29 07:49:56 -0300

    update progressaoAritmeticaV2

--- exercíciosPy/Exercícios Curso em Vídeo/aprendendoCores.py
+++ exercíciosPy/Exercícios Curso em Vídeo/aprendendoCores.py
@@ -1,5 +1,6 @@
 print('\033[0;31;40m ========== CORES NO TERMINAL ==========\033[m')
 nome = str(input('Digite seu nome e sobrenome: '))
+#uso de dicionário para experimentação de cores
 cores = {'limpa':'\033[m',
         'azul':'\033[34m', 
         'amarelo':'\033[33m', 
--- exercíciosPy/Exercícios Curso em Vídeo/repeticoesWhilePython/progressaoAritmeticaV2.py
+++ exercíciosPy/Exercícios Curso em Vídeo/repeticoesWhilePython/progressaoAritmeticaV2.py
@@ -1,10 +1,27 @@
+
 print('========== PROGRESSÃO ARITMÉTICA 2.0 ==========')
 p1 = float(input('Digite o primeiro termo: '))
 raz = float(input('Digite a razão: '))
 roda = 0
+max = 0
+extra = 0
+total = 0
 #progressão aritmética usando While
-while roda < 10:
+print("===== TERMOS DE UMA P.A =====")
+max = int(input('Quantos termos deseja ver: '))
+while roda < max:#roda enquanto não atingir o limite pedido  
+    total += 1      
     print('{:.0f} -> '.format(p1),end='')
-    p1 = p1 + raz
+    p1 = p1 + raz 
     roda += 1
+    if (roda >= max):        
+        print('Pausa')
+        #variável que adiciona novo limite
+        extra = int(input('\nMostrar mais termos? Digite 0 para sair: '))
+        if (extra != 0):
+            roda = 0
+            max = extra            
+        elif (extra == 0):
+            print('Saindo...')            
+print('Você viu {} termos de uma P.A'.format(total))            
 print('FIM')
