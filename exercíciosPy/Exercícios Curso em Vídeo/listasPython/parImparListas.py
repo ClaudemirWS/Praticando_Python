2f83e3e283c70f6064e7d507dc1ab5b3a03e6a57
ClaudemirWS <claudemirwss@gmail.com>
2021-11-10 16:54:43 -0300

    add parImparListas

---
+++ exercíciosPy/Exercícios Curso em Vídeo/listasPython/parImparListas.py
@@ -0,0 +1,24 @@
+listaNum = []
+listaImpar = []
+listaPar = []
+roda = 'S'
+while roda == 'S':
+    num = int(input('Digite um número: '))
+    if (num > 0):
+        listaNum.append(num)
+        print('Adicionando a lista ...')
+        if (num % 2 == 0):
+            listaPar.append(num)
+        elif (num % 2 == 1):
+            listaImpar.append(num)
+    else: 
+        print('Tente outro número.')
+    roda = str(input('Digitar outro número? S/N: ')).strip().upper()[0]
+    if (roda == 'N'):
+        print('Saindo...')
+        break
+listaNum.sort()
+print(f"""Você digitou os números: {listaNum}.
+Os pares foram: {listaPar}.
+Os ímpares foram {listaImpar}.""")
+
