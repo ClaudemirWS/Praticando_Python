0de4031f84ed3cd3d127f28d05a42c6c9875eb96
ClaudemirWS <claudemirwss@gmail.com>
2021-11-15 13:21:17 -0300

    add sorteioDados

---
+++ exercíciosPy/Exercícios Curso em Vídeo/dicionariosPython/sorteioDados.py
@@ -0,0 +1,15 @@
+from random import randint
+from operator import itemgetter
+print('========== SORTEIO DE DADOS =========')
+dcrn = {}
+ranking = []
+for indice in range (1, 5):
+    dcrn[f'jogador {indice}'] = randint(1,6)
+for indice, valor in dcrn.items():
+    print(f'O {indice} tirou o dado {valor}.')
+
+print('========== RANKING DOS JOGADORES ==========')
+ranking = sorted(dcrn.items(), key=itemgetter(1), reverse=True)
+for indice, varreLista in enumerate(ranking):
+    indice += 1
+    print(f'{indice}º lugar: {varreLista[0]} com o dado {varreLista[1]}')
