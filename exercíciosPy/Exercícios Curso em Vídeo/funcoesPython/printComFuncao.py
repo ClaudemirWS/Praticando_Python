33793f1dfa7c769aa2b5b9d9d2345abbe0862ef2
ClaudemirWS <claudemirwss@gmail.com>
2021-11-17 10:13:10 -0300

    add printComFuncao

---
+++ exercíciosPy/Exercícios Curso em Vídeo/funcoesPython/printComFuncao.py
@@ -0,0 +1,8 @@
+print('========== MELHORANDO UM PRINT ==========')
+def escreva(txt):
+    print('-' * len(txt))
+    print(f'{txt}')
+    print('-' * len(txt))
+texto = str(input('Digite algo: '))
+escreva(texto)
+
