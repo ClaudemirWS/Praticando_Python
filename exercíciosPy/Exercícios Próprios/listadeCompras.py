9794419446794332fa5a100877c9e600d255a03c
ClaudemirWS <claudemirwss@gmail.com>
2021-11-20 09:52:44 -0300

    update listadeCompras

--- exercíciosPy/Exercícios Próprios/listadeCompras.py
+++ exercíciosPy/Exercícios Próprios/listadeCompras.py
@@ -1,18 +1,31 @@
 #Em andamento....
 print('========== Lista de Compras =========')
+def mostraLista(listaCompras):
+    print()
+    print('-' * 33)
+    print(f'{"[ COMPRAR ]":>1} {"[ QTD ]":>20}')
+    print('-' * 33)
+    for cod, items in enumerate (listaCompras):
+        for listarProd in items:
+            print(f'{listarProd["item"]} {listarProd["qtd"]:>20}')
+            
+
+def registraItem(dcrn, item):
+    item.append(dcrn.copy())
+    listaCompras.append(item[:])
+    item.clear()
+
 #INICIO DO PROGRAMA
 dcrn = {}
-lista = []
+item = []
 listaCompras = []
 inicia = 'S'
 while (inicia == 'S'):
     dcrn['item'] = str(input('Digite o nome de um item: ')).strip().title()
     dcrn['qtd'] = int(input('Digite a quantidade para comprar: '))
-    lista.append(dcrn.copy())
-    listaCompras.append(lista[:])
-    lista.clear()
+    registraItem(dcrn,item)
     inicia = str(input('Novo item? S/N: ')).strip().upper()[0]
     if (inicia != 'S'):
         print('Saindo...')
         break
-print(lista)
+mostraLista(listaCompras)
