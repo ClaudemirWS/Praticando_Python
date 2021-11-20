bac57ce5a1ca518f99b4d13ff7a1c48ea68da928
ClaudemirWS <claudemirwss@gmail.com>
2021-11-20 00:10:29 -0300

    update listadeCompras

--- exercíciosPy/Exercícios Próprios/listadeCompras.py
+++ exercíciosPy/Exercícios Próprios/listadeCompras.py
@@ -1,12 +1,18 @@
 #Em andamento....
 print('========== Lista de Compras =========')
+#INICIO DO PROGRAMA
+dcrn = {}
 lista = []
-inicia = 1
-while (inicia == 1):
-    item = str(input('Digite o nome de um item: '))
-    qtd = int(input('Digite a quantidade para comprar: '))
-    lista.append({item,qtd}) 
-    inicia = int(input('Novo item? 1 - SIM ou 0 - NÃO: '))
-   
-print("Sua lista: {}".format(lista))
-
+listaCompras = []
+inicia = 'S'
+while (inicia == 'S'):
+    dcrn['item'] = str(input('Digite o nome de um item: ')).strip().title()
+    dcrn['qtd'] = int(input('Digite a quantidade para comprar: '))
+    lista.append(dcrn.copy())
+    listaCompras.append(lista[:])
+    lista.clear()
+    inicia = str(input('Novo item? S/N: ')).strip().upper()[0]
+    if (inicia != 'S'):
+        print('Saindo...')
+        break
+print(lista)
