9f4d35fae4fb69b7aa7e999af04c22168043fffe
ClaudemirWS <claudemirwss@gmail.com>
2021-11-23 09:24:36 -0300

    update folder name and add exercitandoModulos

---
+++ exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/moeda.py
@@ -0,0 +1,15 @@
+def dobro(valor):
+    valor *= 2
+    return valor
+
+def metade(valor):
+    valor /= 2
+    return valor
+
+def aumentar(valor):
+   valor += (valor*10/100)
+   return valor
+
+def diminuir(valor):
+   valor -= (valor*10/100)
+   return valor
