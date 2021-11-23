3efc010d5662e5e34b13f290f2c5099b1f38cdfd
ClaudemirWS <claudemirwss@gmail.com>
2021-11-23 10:09:24 -0300

    update 1 exercitandoModulos

--- exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/moeda.py
+++ exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/moeda.py
@@ -8,8 +8,13 @@ def metade(valor):
 
 def aumentar(valor):
    valor += (valor*10/100)
+   str(valor)
    return valor
 
 def diminuir(valor):
    valor -= (valor*10/100)
    return valor
+
+def moeda(valor):
+    valor =  str(f'R$ {valor:.2f}').replace('.',',')
+    return valor
--- exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/programaPrincipal.py
+++ exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/programaPrincipal.py
@@ -1,7 +1,7 @@
 import moeda
 print('========== EXERCITANDO MODULOS ==========')
 num = float(input('Digite um valor: R$ '))
-print(f"""O dobro de R$ {num} é R$ {moeda.dobro(num)}
-A metade de R$ {num} é R$ {moeda.metade(num)}
-Um aumento de 10% em R$ {num} é R$ {moeda.aumentar(num)}
-Uma redução de 10% em R$ {num} é R$ {moeda.diminuir(num)}""")
+print(f"""O dobro de R$ {moeda.moeda(num)} é {moeda.moeda(moeda.dobro(num))}
+A metade de {moeda.moeda(num)} é {moeda.moeda(moeda.metade(num))}
+Um aumento de 10% é {moeda.moeda(moeda.aumentar(num))}
+Uma redução de 10% é {moeda.moeda(moeda.diminuir(num))}""")
