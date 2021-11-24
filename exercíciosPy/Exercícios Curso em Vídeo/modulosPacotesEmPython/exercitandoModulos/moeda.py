7d2605f021f9aae29c6ca2ada81e5d0050c69904
ClaudemirWS <claudemirwss@gmail.com>
2021-11-24 12:40:29 -0300

    update 3 exercitandoModulos

--- exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/moeda.py
+++ exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/moeda.py
@@ -6,15 +6,24 @@ def metade(valor):
     valor /= 2
     return moeda(valor)
 
-def aumentar(valor):
-   valor += (valor*10/100)
+def aumentar(valor, porc):
+   valor += (valor*porc/100)
    str(valor)
    return moeda(valor)
 
-def diminuir(valor):
-   valor -= (valor*10/100)
+def diminuir(valor, porc):
+   valor -= (valor*porc/100)
    return moeda(valor)
 
 def moeda(valor):
     valor =  str(f'R$ {valor:.2f}').replace('.',',')
     return valor
+
+def resumo(valor, porcAum, porcRed):
+    print('-' * 40)
+    print(f"""O dobro de R$ {moeda(valor)} é {dobro(valor)}
+A metade de {moeda(valor)} é {metade(valor)}
+Um aumento de {porcAum}% é {aumentar(valor, porcAum)}
+Uma redução de {porcRed}% é {diminuir(valor, porcRed)}""")
+    print('-' * 40)
+
--- exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/programaPrincipal.py
+++ exercíciosPy/Exercícios Curso em Vídeo/modulosPacotesEmPython/exercitandoModulos/programaPrincipal.py
@@ -1,7 +1,4 @@
 import moeda
 print('========== EXERCITANDO MODULOS ==========')
 num = float(input('Digite um valor: R$ '))
-print(f"""O dobro de R$ {moeda.moeda(num)} é {moeda.dobro(num)}
-A metade de {moeda.moeda(num)} é {moeda.metade(num)}
-Um aumento de 10% é {moeda.aumentar(num)}
-Uma redução de 10% é {moeda.diminuir(num)}""")
+moeda.resumo(num, 25, 30)
